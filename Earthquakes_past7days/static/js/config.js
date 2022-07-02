36334bb0a0846b862181b9dde08763eade67ac99
mikebowman79 <101996041+mikebowman79@users.noreply.github.com>
2022-07-02 12:47:11 -0400

    Update config.js

--- Earthquakes_past7days/static/js/config.js
+++ Earthquakes_past7days/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoibWVib3dtYW4xIiwiYSI6ImNsNDRhYmdhazAwNGMzZnF1bHlmZXMwbTMifQ.0NjW7VgNJLYTftM9Ra-o2w"
+const API_KEY = "pk.eyJ1IjoibWVib3dtYW4xIiwiYSI6ImNsNDRhYmdhazAwNGMzZnF1bHlmZXMwbTMifQ.0NjW7VgNJLYTftM9Ra-o2w";
